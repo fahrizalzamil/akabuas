b32173da2842ade4f21715a029b80fe2d3a16d48
fahrizalzamil <fahrizalzamil7@gmail.com>
2023-08-11 01:21:06 +0700

    initial commit

---
+++ app/src/main/java/com/uas/notes/Config.java
@@ -0,0 +1,15 @@
+/**
+ * NIM : 10120005
+ * NAMA : Fahrizal Zamil Maripatullah
+ * KELAS : IF-1
+ */
+package com.uas.notes;
+
+public class Config {
+
+    public static String DB_URL = "https://uas-10120005-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+    public static String getDB_URL() {
+        return DB_URL;
+    }
+}
---
+++ app/src/main/java/com/uas/notes/model/NoteCategory.java
@@ -0,0 +1,10 @@
+/**
+ * NIM : 10120005
+ * NAMA : Fahrizal Zamil Maripatullah
+ * KELAS : IF-1
+ */
+package com.uas.notes.model;
+
+public class NoteCategory {
+    public String title, total;
+}
